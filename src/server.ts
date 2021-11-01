c181864445b114f73e7e44f4429ff5a23a0211f9
itmaxxx <dmitrievmaks77@gmail.com>
2021-11-01 18:39:58 +0200

    Added simple upload file form, upload file route, connected formidable

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import * as http from "http";
 import {IncomingMessage, ServerResponse} from "http";
+import * as formidable from "formidable";
 
 http.createServer(function(req: IncomingMessage, res: ServerResponse){
 	const url = req.url;
@@ -7,10 +8,31 @@ http.createServer(function(req: IncomingMessage, res: ServerResponse){
 	// console.log("User-Agent: " + request.headers["user-agent"]);
 	console.log(`[${req.method}] ${url}`);
 
-	if (url === '/') {
-		res.statusCode = 200;
-		res.write('hello world');
+	if (url === '/upload' && req.method === 'POST') {
+		const form = formidable();
+
+		form.parse(req, function(err, fields, files) {
+			if (err) {
+				throw (err);
+			}
+
+			console.log({ fields, files });
+			console.log(files.upload[0]);
+
+			res.writeHead(200);
+			res.write('received upload');
+			res.end();
+		});
+
+		return;
 	}
 
-	res.end();
+	res.writeHead(200, { 'content-type': 'text/html' });
+	res.end(
+			'<form action="/upload" enctype="multipart/form-data" method="post">'+
+			'<input type="text" name="title"><br>'+
+			'<input type="file" name="upload" multiple="multiple"><br>'+
+			'<input type="submit" value="Upload">'+
+			'</form>'
+	);
 }).listen(3000);
