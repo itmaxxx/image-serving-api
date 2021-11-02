ace1936a6b9645d4e976f4ee9e6ef2733db394b2
itmaxxx <dmitrievmaks77@gmail.com>
2021-11-02 12:20:50 +0200

    Serve file util

---
+++ src/utils/serveFile.ts
@@ -0,0 +1,12 @@
+import * as fs from 'fs';
+import {ServerResponse} from "http";
+import {getMimeType} from "./getFileMimeType";
+
+export const serveFile = async (path: string, res: ServerResponse, statusCode: number = 200) => {
+	if(!fs.existsSync(path)) throw ({ message: 'File not found'});
+
+	const readStream = fs.createReadStream(path) ;
+	res.statusCode = statusCode;
+	res.setHeader('Content-Type', getMimeType(path));
+	readStream.pipe(res) ;
+}
