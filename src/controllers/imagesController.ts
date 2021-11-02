085a7328e788139fbe1cfdd9e7b5dc75d295be4a
itmaxxx <dmitrievmaks77@gmail.com>
2021-11-02 12:21:38 +0200

    Moved image upload to imagesController

---
+++ src/controllers/imagesController.ts
@@ -0,0 +1,32 @@
+import * as formidable from "formidable";
+import * as fs from "fs";
+import {sendHttpJsonResponse} from "../utils/sendHttpJsonResponse";
+import {IncomingMessage, ServerResponse} from "http";
+
+export default class ImagesController {
+	public async uploadImage(req: IncomingMessage, res: ServerResponse) {
+		try {
+			const form = new formidable.IncomingForm();
+
+			form.parse(req, function(err, fields, files) {
+				if (err) {
+					throw (err);
+				}
+
+				const file: any = (files.upload as any)?.length ? files.upload[0] : files.upload;
+
+				if (!file) {
+					throw ({ message: 'File not passed' })
+				}
+
+				fs.rename(file._writeStream.path, './src/www/public/uploads/original/' + file.originalFilename, function (err) { throw err; });
+
+				sendHttpJsonResponse(res, 200, { message: "Your file uploaded" })
+			});
+
+			return;
+		} catch (error: any) {
+			sendHttpJsonResponse(res, 500, { message: error.message || "Failed to upload file" })
+		}
+	}
+}
