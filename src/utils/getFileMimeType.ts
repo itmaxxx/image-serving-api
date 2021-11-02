28aab6cabd73d6aac86fe76c338ff98aba6d9235
itmaxxx <dmitrievmaks77@gmail.com>
2021-11-02 12:21:01 +0200

    Get mime type util

---
+++ src/utils/getFileMimeType.ts
@@ -0,0 +1,29 @@
+export const getMimeType = (path: string) => {
+	if(!path) throw({ message: 'Path is empty' });
+
+	const dotPosition = path.lastIndexOf('.');
+
+	if( dotPosition == -1 ) {
+		return 'text/plain' ;
+	}
+
+	const extension = path.substring( dotPosition + 1 ) ;
+	switch( extension ) {
+		case 'html' :
+		case 'css'  :
+			return 'text/' + extension ;
+		case 'jpeg' :
+		case 'jpg'  :
+			return 'image/jpeg' ;
+		case 'bmp'  :
+		case 'gif'  :
+		case 'png'  :
+			return 'image/' + extension ;
+		case 'json' :
+		case 'pdf'  :
+		case 'rtf'  :
+			return 'application/' + extension ;
+		default :
+			return 'text/plain' ;
+	}
+}
