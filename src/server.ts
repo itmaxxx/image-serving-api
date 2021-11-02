edd49eb8e7556604a6706520370d3a4c266777aa
itmaxxx <dmitrievmaks77@gmail.com>
2021-11-02 12:22:32 +0200

    Refactored server.ts, moved upload form to html and served it, implemented imagesController

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import * as http from "http";
 import {IncomingMessage, ServerResponse} from "http";
-import * as formidable from "formidable";
+import ImagesController from "./controllers/imagesController";
 import {sendHttpJsonResponse} from "./utils/sendHttpJsonResponse";
-import * as fs from "fs";
+import {serveFile} from "./utils/serveFile";
 
 const PORT = 3000;
 
@@ -11,38 +11,13 @@ http.createServer(function(req: IncomingMessage, res: ServerResponse){
 
 	console.log(`[${req.method}] ${url}`);
 
+	const imagesController = new ImagesController();
+
 	if (url === '/upload' && req.method === 'POST') {
-		try {
-			const form = new formidable.IncomingForm();
-
-			form.parse(req, function(err, fields, files) {
-				if (err) {
-					throw (err);
-				}
-
-				const file: any = (files.upload as any)?.length ? files.upload[0] : files.upload;
-
-				if (!file) {
-					throw ({ message: 'File not passed' })
-				}
-
-				fs.rename(file._writeStream.path, './src/www/public/uploads/original/' + file.originalFilename, function (err) { throw err; });
-
-				sendHttpJsonResponse(res, 200, { message: "Your file uploaded" })
-			});
-
-			return;
-		} catch (error: any) {
-			sendHttpJsonResponse(res, 500, { message: error.message || "Failed to upload file" })
-		}
+		return imagesController.uploadImage(req, res);
+	} else if (url === '/' && req.method === 'GET') {
+		return serveFile('./src/www/index.html', res);
 	}
 
-	res.writeHead(200, { 'content-type': 'text/html' });
-	res.end(
-			'<form action="/upload" enctype="multipart/form-data" method="post">'+
-			'<input type="text" name="title"><br>'+
-			'<input type="file" name="upload" multiple="multiple"><br>'+
-			'<input type="submit" value="Upload">'+
-			'</form>'
-	);
+	sendHttpJsonResponse(res, 404, { message: 'Route not found' });
 }).listen(PORT);
