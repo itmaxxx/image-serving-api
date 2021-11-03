89a02b048a85782f737b6925649945e5c118d2b5
itmaxxx <dmitrievmaks77@gmail.com>
2021-11-03 20:15:18 +0200

    Move file function which works properly on unix systems

---
+++ src/utils/moveFile.ts
@@ -0,0 +1,15 @@
+import * as fs from 'fs';
+
+export const moveFile = (oldPath: string, newPath: string) => {
+  fs.readFile(oldPath, function (err, data) {
+    if (err) throw err;
+
+    fs.writeFile(newPath, data, function (err) {
+      if (err) throw err;
+    });
+
+    fs.unlink(oldPath, function (err) {
+      if (err) throw err;
+    });
+  });
+}
