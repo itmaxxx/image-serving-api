5c95a62cb12fd013fc52b2cbe37f53e444525a72
itmaxxx <dmitrievmaks77@gmail.com>
2022-01-29 15:05:57 +0200

    Return status code in http json response

--- src/utils/sendHttpJsonResponse.ts
+++ src/utils/sendHttpJsonResponse.ts
@@ -1,10 +1,6 @@
 import { ServerResponse } from 'http';
 
-export const sendHttpJsonResponse = (
-  res: ServerResponse,
-  statusCode: number,
-  data: any
-) => {
+export const sendHttpJsonResponse = (res: ServerResponse, statusCode: number, data: any) => {
   res.writeHead(statusCode, { 'content-type': 'application/json' });
-  res.end(JSON.stringify(data));
+  res.end(JSON.stringify({ ...data, statusCode }));
 };
